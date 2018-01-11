e123d64a5fcc75efd99d2300114ce2261542c619
Rik Schennink <rikschennink@gmail.com>
2018-01-11 10:57:14 +0100

    reformat license

--- ui/component.js
+++ ui/component.js
@@ -1,35 +1,25 @@
 define(function() {
-
     return function(element) {
-        
         console.log('Component mounted on', element);
-        
-    
+
         // logic here
         element.textContent = 'Component mounted';
-    
-    
+
         // public component API
         element.foo = function() {
-    
             console.log('foo called!');
-    
+
             // dispatch events to notify other components
-            element.dispatchEvent( new CustomEvent('bar') );
+            element.dispatchEvent(new CustomEvent('bar'));
         };
-    
-    
+
         // expose destroy method
         return function() {
-            
             // restore content
             element.textContent = '';
-    
+
             // clean up methods
             element.foo = undefined;
-    
         };
-
     };
-
 });
